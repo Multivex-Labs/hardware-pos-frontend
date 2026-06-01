2284ba61ed141f83f6708ba7ca3e6e3882b9221d
Multivex-Labs <patrickrevos2@gmail.com>
2026-06-01 14:20:59 +0300

    Fix Clear All Data URL

--- src/pages/settings.jsx
+++ src/pages/settings.jsx
@@ -4,7 +4,7 @@ import { register } from '../services/api'
 import toast from 'react-hot-toast'
 import axios from 'axios'
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' })
+const API = axios.create({ baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000/api' })
 API.interceptors.request.use(cfg => {
   const token = localStorage.getItem('token')
   if (token) cfg.headers.Authorization = `Bearer ${token}`
